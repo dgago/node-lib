21742e5fb5f3771f688396b55ade8b1d2fe46b0d
Diego Gago <diego.gago@gmail.com>
2018-04-18 10:34:34 -0300

    Adds dist/ directory.

---
+++ dist/__tests__/t8.test.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const index_1 = require("../index");
+test("basic", () => {
+    expect(index_1.sum()).toBe(0);
+});
+test("basic again", () => {
+    expect(index_1.sum(1, 2)).toBe(3);
+});
+//# sourceMappingURL=t8.test.js.map
