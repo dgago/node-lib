38a8fe2b86a1b6f75680180fefe39866b0266ef5
Diego Gago <diego.gago@pedidosya.com>
2021-01-23 16:04:16 -0300

    merge with bitbucket

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ const clean = require("gulp-clean");
 const concat = require("gulp-concat");
 const gulp = require("gulp");
 
-gulp.task("declarations", () => {
+gulp.task("concat-declarations", () => {
   gulp
     .src("./dist/types/**/*.d.ts")
     .pipe(concat("index.d.ts"))
     .pipe(gulp.dest("./dist/"));
+});
 
+gulp.task("clean-declarations", () => {
   gulp.src("./dist/types").pipe(clean());
 });
